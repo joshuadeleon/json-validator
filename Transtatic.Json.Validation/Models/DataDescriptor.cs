22b9140c87a0c8c322d250eba248f0f74ce7f433
Joshua DeLeon <josh@transtatic.com>
2017-02-21 09:13:15 -0800

    Some minor updates

---
+++ Transtatic.Json.Validation/Models/DataDescriptor.cs
@@ -0,0 +1,39 @@
+﻿using NJsonSchema;
+
+namespace Transtatic.Json.Validation.Models {
+	public class DataDescriptor {
+		#region Properties
+		public string DataType { get; set; }
+		public string FieldName { get; set; }
+		public bool IsRequired { get; set; }
+
+		public bool IsNullable { get; set; }
+		public string Pattern { get; set; }
+		#endregion
+
+		#region Constructors
+		public DataDescriptor () { }
+		public DataDescriptor (JsonProperty property, bool isRequired) {
+			if ((property.Type & (JsonObjectType.String | JsonObjectType.Null)) != 0) {
+				DataType = (property.Format == null) ? "string" : property.Format;
+				Pattern = property.Pattern;
+			}
+			else if (property.Type.HasFlag(JsonObjectType.Boolean))
+				DataType = "bool";
+			else
+				DataType = property.Format;
+
+
+			FieldName = property.Name;
+			IsNullable = (property.Type & JsonObjectType.Null) == JsonObjectType.Null 
+								|| property.Type == JsonObjectType.String;
+		}
+		#endregion
+
+		#region Methods
+		public override string ToString() {
+			return $"FieldName: {FieldName} - DataType: {DataType} - Required: {IsRequired} - Nullable: {IsNullable}";
+		}
+		#endregion
+	}
+}
---
+++ Transtatic.Json.Validation/Models/Property.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transtatic.Json.Validation.Interfaces;
+
+namespace Transtatic.Json.Validation.Models {
+	/// <summary>
+	/// Represents a property (field) in the data schema
+	/// </summary>
+	public class Property : IProperty {
+		#region Properties
+		public string Name { get; set; }
+		public string Value { get; set; }
+		#endregion
+
+		#region Constructors
+		public Property(string name, string value) {
+			Name = name;
+			Value = value;
+		}
+		#endregion
+	}
+}
